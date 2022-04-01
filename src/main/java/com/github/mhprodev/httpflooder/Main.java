119927bb4d3358f00ce7efc8b67962622e22b60f
RainbowRain <95581741+InvalidAccount69@users.noreply.github.com>
2022-04-01 17:17:52 +0430

    Added Time Remaining

--- src/main/java/com/github/mhprodev/httpflooder/Main.java
+++ src/main/java/com/github/mhprodev/httpflooder/Main.java
@@ -44,7 +44,7 @@ public class Main {
             while (timer > 0) {
                 timer -= 1;
                 Thread.sleep(1000);
-                System.out.printf("PPS: %d CPS: %d\n", Main.pps, Main.cps);
+                System.out.printf("PPS: %d CPS: %d Time Remaining: %d\n", Main.pps, Main.cps, timer);
                 Main.pps = 0;
                 Main.cps = 0;
             }
